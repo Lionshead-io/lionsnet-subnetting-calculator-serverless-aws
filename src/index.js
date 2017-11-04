5806942198b5d593459d32af774735c3af87ec22
Michael Iglesias <mike.iglesias11@gmail.com>
2017-11-04 15:49:28 -0400

    removed comments from src/index.js. Created misc. test snippet file for easy testing.

--- src/index.js
+++ src/index.js
@@ -236,11 +236,3 @@ exports.main = (event, context, callback) => {
     exports.deleteSubnet(event, context, callback);
   }
 };
-
-// exports.configure({body: JSON.stringify({DefaultWorkspace: '100.64.0.0/10'})}, {}, (err, value) => console.log(value, 'cb'));
-// exports.getConfiguration({}, {}, (err, value) => console.log(value, 'cb'));
-// exports.createVpc({body: JSON.stringify({vpcId: 'w-prod', totalHosts: 512})}, {}, (err, value) => console.log(value, 'cb'));
-// exports.deleteVpc({body: JSON.stringify({vpcId: 'w-prod'})}, {}, (err, value) => console.log(value, 'cb'));
-// exports.createSubnet({pathParameters: JSON.stringify({vpcId: 'w-prod'}), body: JSON.stringify({vpcId: 'w-prod', subnetCount: 2, hostsPerSubnet: 64})}, {}, (err, value) => console.log(err, value, 'cb'));
-// exports.deleteSubnet({pathParameters: JSON.stringify({vpcId: 'w-prod', subnetNetworkAddress: '100.64.1.192'})}, {}, (err, value) => console.log(err, value, 'cb'));
-// exports.getVpc({pathParameters: JSON.stringify({vpcId: 'w-prod'})}, {}, (err, value) => console.log(err, value, 'cb'));
